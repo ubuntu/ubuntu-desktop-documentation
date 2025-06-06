aca65693e22857d05d98fad4c4a240296af62197
Marek Suchánek <marek.suchanek@canonical.com>
2025-06-06 12:02:10 +0200

    Fix the OGP Site URL

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ copyright = "%s CC-BY-SA, %s" % (datetime.date.today().year, author)
 # NOTE: The Open Graph Protocol (OGP) enhances page display in a social graph
 #       and is used by social media platforms; see https://ogp.me/
 
-ogp_site_url = "https://ubuntu-desktop-documentation.readthedocs-hosted.com/"
+ogp_site_url = "https://ubuntu-desktop-documentation.readthedocs-hosted.com/en/latest/"
 
 
 # Preview name of the documentation website
